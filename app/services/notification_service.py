034d08d69f9174d70edd8dcd3b2de0650211c737
1762389546@qq.com <1762389546@qq.com>
2026-02-02 01:10:12 +0800

    1

--- app/services/notification_service.py
+++ app/services/notification_service.py
@@ -39,8 +39,7 @@ class NotificationService:
             <p>系统检测到当前的 AIGC Detector Auth Key 已失效或不存在。</p>
             <p><b>时间:</b> {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}</p>
             <p>请尽快登录服务器或 Redis 更新 <code>{settings.REDIS_AUTH_KEY}</code>。</p>
-            <p>https://ai.lanbeike.online/</p>
-            </p>http://124.221.97.191:9851/admin/login</p>
+
             """
             
             self._send_email(subject, content)
